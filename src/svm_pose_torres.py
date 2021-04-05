f2b43d3b6d89bb2d46e694e648fcb281c5adf8b6
lucianobonzatto <37905647+lucianobonzatto@users.noreply.github.com>
2021-04-05 16:52:35 -0300

    svm para verificar pose

---
+++ src/svm_pose_torres.py
@@ -0,0 +1,64 @@
+# Classification template
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+
+os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
+# Importing the dataset
+dataset = pd.read_csv('/home/lukn23/catkim_ws/src/lidar_samples/datasets/dadosTreinoPose.csv')
+X = dataset.iloc[:, 2: 362].values
+y = dataset.iloc[:, :2].values
+
+print(X)
+
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.compose import ColumnTransformer 
+#labelEncoder_X_1= LabelEncoder()
+#X[:,1] = labelEncoder_X_1.fit_transform(X[:,1])
+#labelEncoder_X_2= LabelEncoder()
+#X[:,2] = labelEncoder_X_2.fit_transform(X[:,2])
+#ct = ColumnTransformer([("OneHot", OneHotEncoder(),[1])], remainder="passthrough") 
+#ct.fit_transform(X)    
+#oneHotEncoder = OneHotEncoder(categorical_features = [1])
+#X= oneHotEncoder.fit_transform(X).toarray()
+#X=X[:,1:]
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train) 
+X_test = sc.transform(X_test)
+
+
+###
+    
+from sklearn import svm
+from sklearn.model_selection import GridSearchCV
+from sklearn.multioutput import MultiOutputRegressor
+
+
+parameters = {'C':[1], 'epsilon':[0.2], 'gamma':["scale"]}
+svr = svm.SVR()
+clf = GridSearchCV(svr, parameters)
+mor = MultiOutputRegressor(clf)
+#clf.fit(X_train, y_train)
+
+mor = mor.fit(X_train, y_train)
+y_pred = mor.predict(X_test)
+
+
+#Evaluating
+# Making the Confusion Matrix
+#from sklearn.metrics import confusion_matrix
+#cm = confusion_matrix(y_test, y_pred)
+
+#print(cm)
+
+###
